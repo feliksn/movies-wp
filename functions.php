e4b4e6671f18550638e417190271164819fabdcb
Feliks N <fnow01@gmail.com>
2024-07-29 13:45:12 +0200

    Скрыть страницу комментариев в админ панеле WP

--- functions.php
+++ functions.php
@@ -6,12 +6,10 @@
  */
 
     add_action( 'wp_enqueue_scripts', 'movies_wp_scripts' );
-
     function movies_wp_scripts() {
         // Styles
         wp_enqueue_style('bootstrap', get_template_directory_uri() . '/lib/bootstrap/bootstrap.min.css');
         wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css');
-
         // Scripts
         wp_deregister_script('jquery');
         wp_enqueue_script('jquery', get_template_directory_uri() . '/lib/jquery/jquery.min.js', array(), false, true);
@@ -21,11 +19,15 @@
     }  
 
     // добавляем чтобы в админке при создании поста в правом меню появилось подмееню сознание миниатюры
+    add_action( 'after_setup_theme', 'movies_wp_post_thumbnails' );
     function movies_wp_post_thumbnails()
     {
         add_theme_support('post-thumbnails');
     }
-    add_action( 'after_setup_theme', 'movies_wp_post_thumbnails' );
 
+    // Удаляем страницу комментариев из админ панели (пока что нам не нужна)
+    add_action('admin_menu', function () {
+        remove_menu_page('edit-comments.php');
+    });
 
 
