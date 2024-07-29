a1880409a421ece3e793072abe9c1f10a54a8fa4
Feliks N <fnow01@gmail.com>
2024-07-29 13:49:57 +0200

    Поправить html-разметку для страницы отдельного фильма

--- single.php
+++ single.php
@@ -8,39 +8,31 @@ get_header(); // подключаем header.php ?>
 
 <section>
 	<div class="container py-4">
-    <div class="row">
-        <div class="col-3 offset-1">
-			<!-- вывод картинки films-->
-			<?php the_post_thumbnail ();?>
-        </div>
-        <div class="col-7">
-			<!-- Назавание фильма -->
-            <h1>
+		<div class="row">
+			<div class="col-3 offset-1">
+				<!-- вывод картинки отдельного фильма -->
+				<img src="<?php the_post_thumbnail_url() ?>" alt="" class="d-block w-100">
+			</div>
+			<div class="col-7">
 				<!-- Назавание фильма -->
-				<?php the_title(); ?>
-			<br>
-			</h1>
-			
-            <p class="fs-4 text-secondary">
- 				<!--Вывод года фильма -->
-				 (<?php echo get_post_meta($post->ID, 'year', true); ?>)
-			</p>
-            <p>
-				<!-- вывод жанров films -->
-				<?php wp_list_categories(['style'=> 'none', 'separator'=> '']); ?>
-            </p>
-            <p>
-				<!-- вывод cast films -->
-				<?php the_tags('', ' ', ''); ?>
-            </p>
-            <p>
-				<!-- вывод описания films -->
-				<?php echo get_post_meta($post->ID, 'extract', true); ?>
-			</p>
-        </div>
-    </div>
-</div>
-
+				<h1><?php the_title(); ?><br></h1>
+				<p class="fs-4 text-secondary"> 
+					<!--Вывод года фильма --> 
+					(<?php echo get_post_meta($post->ID, 'movie_year', true); ?>)
+				</p>
+				<p class="fw-semibold">
+					<!-- вывод жанров отдельного фильма -->
+					<!-- Функция wp_list_categories() - показывает все существующие категории, а не отдельного фильма
+					На странице отдельного фильма нужно использовать the_category() - эта фукнция получает категории/жарны только для отдельного взятого фильма -->
+					Genres: <?php the_category(' '); ?>
+				</p>
+				<!-- вывод актеров отдельного фильма -->
+				<p class="fw-light"><em><?php the_tags('Cast: ', ' ', ''); ?></em></p>
+				<!-- вывод описания/контента отдельного фильма -->
+				<p><?php the_content() ?></p>
+			</div>
+		</div>
+	</div>
 </section>
 
 <?php get_footer(); // подключаем footer.php ?>
