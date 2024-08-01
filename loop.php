3971994c5a61e5f3d81191914030e1ed0cc76064
vieri <egordohlyj4@gmail.com>
2024-08-01 13:34:36 +0300

    add func, in the absence of elem

--- loop.php
+++ loop.php
@@ -35,29 +35,60 @@
                 $tagsStringCutLastComma = substr($tagsString, 0, strlen($tagsString) - 2);
                 $actors = cutString($tagsStringCutLastComma, 30);
             }
+
+            // Формируем отрывок описания для отдельного фильма
+            $fragment = get_the_excerpt();
+            if($fragment == ""){
+                $extract = 'No extract!';
+            }else{
+                $extract = cutString($fragment);
+            }
+
+            // Формируем название, заголовок для отдельного фильма
+            $heading = get_the_title(); 
+            if($heading == ""){
+                $title = 'No title!';
+            }else{
+                $title = $heading;
+            }
+            // Формируем год, для отдельного фильма
+            $arbitraryField = get_post_meta($post->ID, 'movie_year', true);
+            if($arbitraryField == ""){
+                $year = 'No year!';
+            }else{
+                $year = $arbitraryField;
+            }
+            // Формируем картинку, для отдельного фильма
+            $thumbnailUrl = get_the_post_thumbnail_url();
+            if($thumbnailUrl == ""){
+                $thumbnail = 'http://movies-wp/wp-content/uploads/2024/07/movie-default-1.png';
+            }else{
+                $thumbnail = $thumbnailUrl;
+            }
+           
         ?>
         
         <div class="col">
             <div class="card border border-0 shadow-sm">
                 <!-- вывод картинки -->
-                <img src="<?php the_post_thumbnail_url() ?>" alt="Movie thumbnail" class="card-img-top">    
+                <img src="<?php echo $thumbnail; ?>" alt="Movie thumbnail" class="card-img-top">    
                 <!-- вывод жанров, это есть в админке рубрики -->
                 <div class="card-header border border-0"><?php echo $genres; ?></div>
                 <div class="card-body">
                     <h5 class="card-title mb-3">
                         <!-- Назавание фильма -->
                         <!-- "the..." - указывает нам на получение названия отдельной записи/фильма в цикле -->
-                        <?php the_title(); ?>
+                        <?php echo $title; ?>
                         <!--Год фильма -->
                         <small class="text-body-tertiary">
-                            (<?php echo get_post_meta($post->ID, 'movie_year', true); ?>)
+                            (<?php echo $year; ?>)
                         </small>
                     </h5>
                     <!-- Актеры фильма -->
                     <h6 class="card-text mb-3 text-secondary"><em><?php echo $actors; ?></em> </h6>
                     <!-- Описание фильма -->
                     <!-- get_the_excerpt() - это функция получения отрывка записи. Можно найти в админ панеле под картинкой записи/фильма. Это тектстовое поле, содержащее краткое описание записи. -->
-                    <p class="card-text"><?php echo cutString(get_the_excerpt()); ?></p>
+                    <p class="card-text"><?php echo $extract; ?></p>
                     <a href="<?php the_permalink();?>" class="btn btn-primary">Read more...</a>
                 </div>
             </div>
