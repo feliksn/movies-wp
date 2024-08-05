586f939b42db3d6787d657b13b91d4fbaf62f508
vieri <egordohlyj4@gmail.com>
2024-08-06 02:10:13 +0300

    out data in single-movie

--- single.php
+++ single.php
@@ -6,30 +6,55 @@
  */
 get_header(); // подключаем header.php ?>
 
+<?php
+	$thumbnailUrl_single = get_the_post_thumbnail_url() ?: getDefaultThumbnailUrl();
+	$title_single = the_title('', '', false) ?: "No title!";
+	$year_single = get_post_meta($post->ID, 'movie_year', true) ?: "No year!";
+	$extract_single = get_the_excerpt() ?: '<u><b> No extract! </u></b>';
+	
+?>
+
 <section>
 	<div class="container py-4">
 		<div class="row">
 			<div class="col-3 offset-1">
 				<!-- вывод картинки отдельного фильма -->
-				<img src="<?php the_post_thumbnail_url() ?>" alt="" class="d-block w-100">
+				<img src="<?php echo $thumbnailUrl_single; ?>" alt="" class="d-block w-100">
 			</div>
 			<div class="col-7">
 				<!-- Назавание фильма -->
-				<h1><?php the_title(); ?><br></h1>
+				<h1><?php echo $title_single; ?><br></h1>
 				<p class="fs-4 text-secondary"> 
 					<!--Вывод года фильма --> 
-					(<?php echo get_post_meta($post->ID, 'movie_year', true); ?>)
+					(<?php echo $year_single; ?>)
 				</p>
 				<p class="fw-semibold">
 					<!-- вывод жанров отдельного фильма -->
 					<!-- Функция wp_list_categories() - показывает все существующие категории, а не отдельного фильма
 					На странице отдельного фильма нужно использовать the_category() - эта фукнция получает категории/жарны только для отдельного взятого фильма -->
-					Genres: <?php the_category(' '); ?>
+					Genres: 
+					<?php if( has_category(array(1, 'No genres!'))) { ?>
+						<u>No genres!</u>
+					<?php }else{ 
+						 the_category(' '); } 
+					?>
 				</p>
 				<!-- вывод актеров отдельного фильма -->
-				<p class="fw-light"><em><?php the_tags('Cast: ', ' ', ''); ?></em></p>
+				<?php if(has_tag()){ ?>
+					<p class='fw-light'><em> <?php the_tags('Cast: ', ' ', ''); ?></em></p>
+				<?php }else{ ?>
+					<p class='fw-light'><em> Cast: <u><b> No casts! </b></u> </em></p>
+				<?php } ?>
+
 				<!-- вывод описания/контента отдельного фильма -->
-				<p><?php the_content(); ?></p>
+				<p>
+					<?php if(has_excerpt()){ 
+								echo get_the_excerpt();
+						  }else{
+					?>
+						  <u><b> No extract! </u></b>
+					<?php }?>
+				</p>
 			</div>
 		</div>
 	</div>
