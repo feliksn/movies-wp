33a7080b5267cf93f66e788d8d162f2e47c900cc
Feliks N <fnow01@gmail.com>
2024-08-07 09:30:01 +0200

    Сократить код без утраты функ-сти с комментариями

--- single.php
+++ single.php
@@ -7,11 +7,46 @@
 get_header(); // подключаем header.php ?>
 
 <?php
-	$thumbnailUrl_single = get_the_post_thumbnail_url() ?: getDefaultThumbnailUrl();
-	$title_single = the_title('', '', false) ?: "No title!";
-	$year_single = get_post_meta($post->ID, 'movie_year', true) ?: "No year!";
-	$extract_single = get_the_excerpt() ?: '<u><b> No extract! </u></b>';
+	$thumbnailUrl	= get_the_post_thumbnail_url() ?: getDefaultThumbnailUrl();
+	$title			= get_the_title() ?: "No title!";
+	$year			= get_post_meta($post->ID, 'movie_year', true) ?: "No year!";
+	$content 		= get_the_content() ?: "No content!";
 	
+	// Формируем актеров для фильма
+	$actors = '';
+	// Проверяем актеров для фильма
+	if (has_tag()) {
+		$tags = get_the_tags();
+		// Чтобы проверить, что записано в $tags, то можно вызвать функцию raw($tags);
+		// raw($tags, "Актеры");
+		// Если есть актеры, то формируем ссылки и добавляем по очереди в переменную актеров
+		foreach ($tags as $tag) {
+			$actors .= '<a href="' . get_tag_link($tag->term_id).'">' .  $tag->name . "</a> ";
+		}
+	} else {
+		// Если нет актеров, то записываем в переменную актеров простой текст
+		$actors = 'No actors!';
+	}
+	
+	// Формируем жанры для фильма
+	$genres = '';
+	$categories = get_the_category();
+	// Чтобы проверить что записано в $categories, то можно вызвать функцию raw($categories)
+	// raw($categories, "Жанры");
+	// Т.к. в WP жанр/рубрика есть всегда, то создаем жанр "No genres" для фильмов без жанров.
+	// проходимся по каждому жанру для фильма
+	foreach($categories as $category){
+		// Создаем условие для жанра "No genres", чтобы фильтровать фильмы без жанров
+		if($category->term_id == 8){
+			// Если ловим id жанра "No genres" то записываем (не добавляем, а записываем  - не пишем ".=" а пишем "=" ) в переменную просто название жанра без каких-либо ссылок. 
+			$genres = $category->name . "!";
+			// После того как словили жанр "No genres", записываем в переменную только имя жанра и останавливаем цикл "break;", т.к. дальнейшая работа цикла безсмысленна. И даже если у фильма будут другие жанры, то данное условие и так не покажет другие жанры, т.к. есть жанр "No genres" и без смысла показывать другие жанры с жанром "No genres"
+			break;
+		} else {
+			// Если условие не выполняется, т.е. фильм не содержит жанр "No genres", то формируем ссылки жанров и добавляем по очереди в переменную жанров
+			$genres .= '<a href="' . get_category_link($category->term_id) .'">'.  $category->name . "</a> ";
+		}
+	};
 ?>
 
 <section>
