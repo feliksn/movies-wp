f6bb259b14f3c10be7873b230824d4cafce1b184
vieri <egordohlyj4@gmail.com>
2024-08-08 16:41:30 +0300

    вывод жанров в 4 колонки на странице

--- page-genres.php
+++ page-genres.php
@@ -1,15 +1,27 @@
 <?php get_header(); ?>
 
+
+<?php 
+$genres = get_categories(['hide_empty' => 0, 'exclude' => '1']);
+$rowsLen = ceil(count($genres) / 4);
+$genresCols = array_chunk($genres, $rowsLen);
+$colsOrderSM = [0, 2, 1, 3];
+
+?>
+
 <div class="container py-3">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4">
-       
-            <div class="col">
-                <ul>
-                    <?php  wp_list_categories(array('exclude' => '1', 'hide_empty' => 0, 'style' => 'none')) ?>
-                </ul>
+        <?php foreach ($genresCols as $colIndex => $genres) { ?>
+            <div class="col <?php echo "order-sm-$colsOrderSM[$colIndex] order-lg-$colIndex" ?>">
+                <?php foreach ($genres as $genre) { ?>
+                    <a href="<?php echo $genre->slug ?>" class="fs-5 link-offset-1 link-underline link-underline-opacity-25">
+                        <?php echo $genre->name; ?>
+                    </a><br>
+                <?php } ?>
             </div>
-   
+        <?php } ?>
     </div>
 </div>
+
 
 <?php get_footer(); ?>
