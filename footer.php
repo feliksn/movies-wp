c09240e6b76887e249181c7dd1f307b3d964f1fc
vieri <egordohlyj4@gmail.com>
2024-08-11 12:48:45 +0300

    добавлена пагинация

--- footer.php
+++ footer.php
@@ -10,6 +10,13 @@
 		</div>
 	</footer>
 
+	<?php	the_posts_pagination([
+		'type'         => 'list',
+		'prev_text'          => '<span aria-hidden="true">&laquo;</span>',
+		'next_text'          => '<span aria-hidden="true">&raquo;</span>',
+
+	]) ; ?>
+
 	<?php wp_footer(); // необходимо для работы плагинов и функционала  ?>
 </body>
 </html>
