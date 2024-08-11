4c53a81febe85316f2d2d2ea5835856a3822e822
vieri <egordohlyj4@gmail.com>
2024-08-11 13:22:58 +0300

    добавлены некоторые стили к пагинации

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // custom scripts
+$(document).ready(function() { // Start work jQuery--------------------->>>>>>>>>>>>>>>>>>>>
+
+    $('#navpag ul').attr('class', 'pagination justify-content-center');
+    $('#navpag ul li').attr('class', 'page-item');
+    $('#navpag ul li a').attr('class', 'page-link');
+    $('#navpag ul li span.dots').attr('class', 'page-link');
+    $('#navpag ul li span.current').wrap('<a class="page-link active"></a>');
+  
+
+}); // END work jQuery------------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
