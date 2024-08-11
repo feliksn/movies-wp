00d6eeb622ba9ef0a63ac23762970092db7e5909
vieri <egordohlyj4@gmail.com>
2024-08-11 12:43:34 +0300

    добавлена форма поиска

--- searchform.php
+++ searchform.php
@@ -5,3 +5,10 @@
  * @subpackage movies-wp
  */
 ?>
+
+<form class="d-flex" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
+	<label class="screen-reader-text" for="s">Поиск: </label>
+	<input class="form-control me-2" type="search" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search" aria-label="Search"/>
+	<input class="btn btn-outline-success" type="submit" id="searchsubmit" value="Search" />
+</form>
+
