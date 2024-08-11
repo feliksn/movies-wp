a393ed0dd20a823e2facd3d183399324fb2255e5
vieri <egordohlyj4@gmail.com>
2024-08-11 12:45:39 +0300

    доюавлен ыывод на страницу то что искали

--- search.php
+++ search.php
@@ -6,9 +6,15 @@
  */
 get_header(); // подключаем header.php ?> 
 
-<section>
-	<div class="container">
-	</div>
-</section>
+	<section>
+		<div class="container">
+			<h3><?php single_cat_title(); ?></h3>
+			<div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3 g-3">
+				<?php get_template_part('loop'); ?>
+			</div>
+		</div>
+	</section>
+
+
 
 <?php get_footer(); // подключаем footer.php ?>
