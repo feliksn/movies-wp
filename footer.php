a5d9f4b266ec5ece420a0d7a03a4b829adeb0fa9
vieri <egordohlyj4@gmail.com>
2024-08-11 18:36:44 +0300

    вид кода поправка

--- footer.php
+++ footer.php
@@ -12,9 +12,8 @@
 
 	<?php	the_posts_pagination([
 		'type'         => 'list',
-		'prev_text'          => '<span aria-hidden="true">&laquo;</span>',
-		'next_text'          => '<span aria-hidden="true">&raquo;</span>',
-
+		'prev_text'    => '<span aria-hidden="true">&laquo;</span>',
+		'next_text'    => '<span aria-hidden="true">&raquo;</span>'
 	]) ; ?>
 
 	<?php wp_footer(); // необходимо для работы плагинов и функционала  ?>
