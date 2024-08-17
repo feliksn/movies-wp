e529dd4fbbc25170f6f840f6ec24608e72c290c3
Feliks N <fnow01@gmail.com>
2024-08-17 13:56:07 +0200

    Упростить код, ипользуя спец. метод доб. класса в jq

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // custom scripts
 $(document).ready(function() { // Start work jQuery--------------------->>>>>>>>>>>>>>>>>>>>
 
-    $('#navpag ul').attr('class', 'pagination justify-content-center');
-    $('#navpag ul li').attr('class', 'page-item');
-    $('#navpag ul li a').attr('class', 'page-link');
-    $('#navpag ul li span.dots').attr('class', 'page-link');
-    $('#navpag ul li span.current').wrap('<a class="page-link active"></a>');
-    $('#navpag ul li span.page-link').parent().attr('class', 'page-item disabled');
+    $('#navpag ul').addClass('pagination justify-content-center');
+    $('#navpag li').addClass('page-item');
+    $('#navpag a').addClass('page-link');
+    $('#navpag .dots').addClass( 'page-link');
+    $('#navpag .current').wrap('<a class="page-link active"></a>');
+    $('#navpag span.page-link').parent().addClass('page-item disabled');
 
 }); // END work jQuery------------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
